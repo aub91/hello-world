22509f8abdb90c4192f1a2ac9d3a4d6c9d55101a
aub91 <30404017+aub91@users.noreply.github.com>
2017-07-24 11:34:15 +0200

    upload Aubin Project

---
+++ CommandeConsommateurDataSet.cs
@@ -0,0 +1,16 @@
+﻿namespace Fr.Afcepf.Anarmorique.Aubin {
+    
+    
+    public partial class CommandeConsommateurDataSet {
+        partial class DetailProduitDataTable
+        {
+        }
+    }
+}
+
+namespace Fr.Afcepf.Anarmorique.Aubin.CommandeConsommateurDataSetTableAdapters {
+    
+    
+    public partial class COM_CONSOTableAdapter {
+    }
+}
