639521727455e481a8c1429271a06626c67ef819
aub91 <30404017+aub91@users.noreply.github.com>
2017-07-24 11:36:13 +0200

    Add files via upload

---
+++ BarreChoixCompteProducteur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fr.Afcepf.Anarmorique.Aubin.UC
+{
+    public partial class BarreChoixCompteProducteur : UserControl
+    {
+        public BarreChoixCompteProducteur()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
